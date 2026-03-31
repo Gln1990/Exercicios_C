72ca2909bc1a886ab6dace71d2cb17a3d0777169
Gleyson Canindé do Rosario <gleyson01rosario@gmail.com>
2026-03-31 16:05:40 -0300

    Laço_while

---
+++ Laco_while/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*Algoritimo
+  1 - Criar variavel para servir como contador com valor inicial 1
+  2 - Enquanto o valor do contador for menor ou igual a 5, procesar os passos 3, 4 e 5
+  3 - Ler os dois valores
+  4 - Efetuar o calculo, e guardando o resultado na varial R
+  5 - Apresentar o valor calculado em R
+  6 - Acrescentar ao contador 1
+  7 - Quando o contador for maior que 5, encerrar o processamento*/
+int main()
+{
+    int a, b, r, i;
+    i = 1;
+    while(i <= 5){
+        printf("\nInforme o primeiro valor: ");
+        scanf("%d", &a);
+        printf("Informe o segundo valor: ");
+        scanf("%d", &b);
+        r = a + b;
+        printf("\nA soma entre %d e %d e: %d\n", a, b, r);
+        i++;
+    }
+    return 0;
+}
