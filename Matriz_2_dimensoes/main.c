5187128fd90d2d46a0be5db38ce9b959c3fb2432
Gleyson Canindé do Rosario <gleyson01rosario@gmail.com>
2026-03-31 18:13:43 -0300

    Matriz_2_dimensoes

---
+++ Matriz_2_dimensoes/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* Calculo de media de 8 alunos com 4 notas */
+int main()
+{
+    float MD[8][4], num;
+    int i, j;
+
+    printf("znLeitura e apresentacao de notas\n");
+
+    /* Entrada das notas */
+    for(i = 0; i <= 7; i++){
+        printf("\nInforme aas notas do %do. aluno: \n\n", i+1);
+        for(j = 0; j <= 3; j++){
+            printf("Nota %d: ", j+1);
+            scanf("%f", &num);
+            MD[i][j] = num;
+        }
+    }
+
+    /* Saida das notas */
+    for(i = 0; i <= 7; i++){
+        printf("\nAs notas do aluno %d sao: \n\n", i+1);
+        for(j = 0; j <= 3; j++)
+            printf("Notas %d: %.2f\n", j+1, MD[i][j]);
+    }
+    return 0;
+}
